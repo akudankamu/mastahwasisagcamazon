929b900be7df938af43f40b1a64b91856f42375c
arya <sedekahin@gmail.com>
2013-05-28 13:53:46 +0700

    spp vid template

--- wp-content/themes/simplefast-responsive/footer.php
+++ wp-content/themes/simplefast-responsive/footer.php
@@ -4,9 +4,22 @@
 <?php $pages = wp_list_pages('depth=1&title_li=&echo=0');
 $pages2 = preg_split('/(<li[^>]*>)/' ,$pages);foreach($pages2 as $var){
 echo str_replace('</li>', '', $var);}?><br/>
-Copyright &#169; <?php echo date("Y") ?>  <a href="<?php echo home_url() ; ?>"><?php bloginfo('name'); ?></a>, All trademarks are the property of the respective trademark owners. <br/>
+Copyright &#169; 2012  <a href="<?php echo home_url() ; ?>"><?php bloginfo('name'); ?></a>, All trademarks are the property of the respective trademark owners. <br/>
 </footer>
 </div>
+<!-- Place this code above the closing body tag of the page. Note: An Overlay cannot be placed on top of an auto play or over a video player which is smaller than 320x280  -->
+<script type="text/javascript">
+var adParams = {serverdomain: "ads.adk2.com", a: "11698001",overlayEls: "ytIEmbed,ytIFrame"   };
+</script>
+<script type="text/javascript" src="http://cdn.adk2.com/adstract/scripts/overlay/overlay.js"></script>
+<!-- Place this code above the closing body tag of the page -->
+<script type="text/javascript">
+var adParams = {
+  a: '11698001', numOfTimes: '1',duration: '1',serverdomain: 'ads.adk2.com' ,period: 'Minute' 
+};
+</script>
+<script type="text/javascript" src="http://cdn.adk2.com/adstract/scripts/popunder/popunder.js"></script>
+
 <script type="text/javascript">
   var vglnk = { api_url: '//api.viglink.com/api',
                 key: 'c5d8f82b94ccf34b3c04d4ff2e0d11cb' };
